7dea24179927afd31e91ce43639fb8b4cf5c7f0d
Alan Lima da Silva Guerra <alandasilvaguerra@proton.me>
2024-09-15 23:13:54 -0300

    Adicionando sessão home

---
+++ src/app/components/HomeSection.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { Container } from "react-bootstrap";
+import Perfil from "../assets/eu.png";
+import eu from "./Eu";
+
+const HomeSection = () => {
+  return (
+    <section id="home" className="text-center py-5 bg-light">
+      <Container>
+        <div className="d-flex justify-content-center mb-4">
+          <Image
+            src={Perfil}
+            alt="Minha Foto"
+            width={300}
+            height={300}
+            className="rounded-circle shadow"
+          />
+        </div>
+        <h1 className="mt-4 mb-2 text-primary">Olá, eu sou {eu.nome}</h1>
+        <h2 className="mb-3">{eu.title}</h2>
+        <p className="lead text-muted">{eu.description}</p>
+      </Container>
+    </section>
+  )
+};
+
+export default HomeSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,16 @@
 "use client";
 
 import { Container } from 'react-bootstrap';
-import Image from 'next/image';
-import Perfil from './assets/eu.png';
 import ContactSection from './components/ContactSection';
-import eu from './components/Eu';
 import TechnologyCarousel from './components/Carousel';
 import ProjectSection from './components/ProjectSection';
+import HomeSection from './components/HomeSection';
 
 export default function Home() {
   return (
     <>
       {/* Seção Home */}
-      <section id="home" className="text-center py-5 bg-light">
-        <Container>
-          <div className="d-flex justify-content-center mb-4">
-            <Image
-              src={Perfil}
-              alt="Minha Foto"
-              width={300}
-              height={300}
-              className="rounded-circle shadow"
-            />
-          </div>
-          <h1 className="mt-4 mb-2 text-primary">Olá, eu sou {eu.nome}</h1>
-          <h2 className="mb-3">{eu.title}</h2>
-          <p className="lead text-muted">{eu.description}</p>
-        </Container>
-      </section>
+      <HomeSection />
 
       {/* Seção Desenvolvimento */}
       <section id="development" className="py-5 bg-light">
