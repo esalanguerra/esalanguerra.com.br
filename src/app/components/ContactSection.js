2aff807fba11fb86953df198f7619d8e9f71269a
Alan Lima da Silva Guerra <alandasilvaguerra@proton.me>
2024-09-15 21:36:59 -0300

    Adicionando icones de redes sociais

--- src/app/components/ContactSection.js
+++ src/app/components/ContactSection.js
@@ -1,4 +1,5 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { Linkedin, Github, Instagram, Twitter, Newspaper } from 'react-bootstrap-icons';
 
 const ContactSection = () => {
   return (
@@ -6,7 +7,27 @@ const ContactSection = () => {
       <Container>
         <h2>Contato</h2>
         <p>Entre em contato conosco através do formulário abaixo ou envie um e-mail para contato@exemplo.com.</p>
-        {/* Adicione um formulário de contato aqui */}
+        
+        {/* Adicionando ícones de redes sociais */}
+        <Row className="text-center mt-4">
+          <Col>
+            <a href="https://www.linkedin.com/in/seu-perfil" target="_blank" rel="noopener noreferrer" className="text-dark">
+              <Linkedin size={24} className="mx-2" />
+            </a>
+            <a href="https://github.com/seu-perfil" target="_blank" rel="noopener noreferrer" className="text-dark">
+              <Github size={24} className="mx-2" />
+            </a>
+            <a href="https://www.instagram.com/seu-perfil" target="_blank" rel="noopener noreferrer" className="text-dark">
+              <Instagram size={24} className="mx-2" />
+            </a>
+            <a href="https://twitter.com/seu-perfil" target="_blank" rel="noopener noreferrer" className="text-dark">
+              <Twitter size={24} className="mx-2" />
+            </a>
+            <a href="https://tabnews.com.br/seu-perfil" target="_blank" rel="noopener noreferrer" className="text-dark">
+              <Newspaper size={24} className="mx-2" />
+            </a>
+          </Col>
+        </Row>
       </Container>
     </section>
   )
