0361800899c30f1dda6b93894a81f757b74d4853
Alan Lima da Silva Guerra <esalanguerra@gmail.com>
2024-10-28 15:04:34 -0300

    alteerando imagens

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/eu.jpeg")}
+        src={getImageUrl("hero/heroImage.jpeg")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
