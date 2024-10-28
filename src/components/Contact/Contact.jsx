ac5c3279a0b67ca9900340e34d9ef01aaa4c840c
Alan Lima da Silva Guerra <esalanguerra@gmail.com>
2024-10-28 15:38:29 -0300

    informações de contatos

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,24 +7,24 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Contatos</h2>
+        <p>Entre em Contato e faça uma reunião</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:contato@esalanguerra.com.br">contato@esalanguerra.com.br</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/esalanguerra">linkedin.com/esalanguerra</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://www.github.com/esalanguerra">github.com/esalanguerra</a>
         </li>
       </ul>
     </footer>
